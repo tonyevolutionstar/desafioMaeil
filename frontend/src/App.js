18e04c18f23f7a0681cd7018cd11e1cd4c0704fc
ANTÓNIO RAMOS <tonyantonio@outlook.pt>
2023-06-12 18:12:21 +0100

    change layout, add export

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,51 @@
 import './App.css';
-import { read, utils }  from 'xlsx'; //https://docs.sheetjs.com/docs/getting-started/installation/nodejs
-import React, { useState } from "react";
+import { read, utils, writeFile }  from 'xlsx'; //https://docs.sheetjs.com/docs/getting-started/installation/nodejs
+import React, { useMemo, useCallback, useState, useEffect } from "react";
 import { AgGridReact } from 'ag-grid-react';
 
-
 function App() {
-  const [items, setItems] = useState([])
+  const [items, setItems] = useState([{editable: true}])
   
   const columns = [ 
-    { field: 'Referencia', width:'120', resizable: true, sortable: true, filter: 'agSetColumnFilter'},
-    { field: 'Cliente', width:'120', resizable: true, sortable: true },
-    { field: 'Estado', width:'90',  resizable: true, sortable: true },
-    { field: 'Tipo', width:'90', resizable: true, sortable: true },
-    { field: 'Matricula', width:'120', resizable: true, sortable: true },
-    { field: 'TipoCarga', width:'120', resizable: true, sortable: true },
-    { field: 'Prioridade', width:'120', resizable: true, sortable: true },
-    { field: 'DataRegisto', width:'120', resizable: true, sortable: true },
-    { field: 'BlockedTime', width:'120', resizable: true, sortable: true },
-    { field: 'POD', width:'90', resizable: true, sortable: true },
-    { field: 'Parque', width:'120', resizable: true, sortable: true },
-    { field: 'TipoEquipamento', width:'120', resizable: true, sortable: true },
-    { field: 'DepotIdBlocking', width:'120', resizable: true, sortable: true },
-    { field: 'DataAtribExp', width:'120', resizable: true, sortable: true },
-    { field: 'Vessel', width:'90', resizable: true, sortable: true },
-    { field: 'Voyage', width:'90', resizable: true, sortable: true },
-    { field: 'POL', width:'90', resizable: true, sortable: true }
+    { field: 'Referencia', width:130, resizable: true, sortable: true, filter: 'agSetColumnFilter'},
+    { field: 'Cliente', width:120, resizable: true, sortable: true },
+    { field: 'Estado', width:90,  resizable: true, sortable: true },
+    { field: 'Tipo', width:90, resizable: true, sortable: true },
+    { field: 'Matricula', width:120, resizable: true, sortable: true },
+    { field: 'TipoCarga', width:120, resizable: true, sortable: true },
+    { field: 'Prioridade', width:120, resizable: true, sortable: true },
+    { field: 'DataRegisto', width:120, resizable: true, sortable: true },
+    { field: 'BlockedTime', width:120, resizable: true, sortable: true },
+    { field: 'POD', width:90, resizable: true, sortable: true },
+    { field: 'Parque', width:120, resizable: true, sortable: true },
+    { field: 'TipoEquipamento', width:120, resizable: true, sortable: true },
+    { field: 'DepotIdBlocking', width:120, resizable: true, sortable: true },
+    { field: 'DataAtribExp', width:120, resizable: true, sortable: true },
+    { field: 'Vessel', width:90, resizable: true, sortable: true },
+    { field: 'Voyage', width:90, resizable: true, sortable: true },
+    { field: 'POL', width:90, resizable: true, sortable: true }
   ];
+
+  const defaultColDef = useMemo(() => {
+    return {
+      sortable: true,
+      filter: true,
+      resizable: true,
+      editable: true,
+      flex: 1,
+    };
+  }, []);
+
+  useEffect(() => {
+    setItems(items)
+  })
+
+  const handleOnExport = () => {
+    var wb = utils.book_new(),
+    ws = utils.json_to_sheet(items);
+    utils.book_append_sheet(wb, ws, "Bookings");
+    writeFile(wb, "excel1_31-12-2020_updated.xlsx");
+  }
 
   const handleFile = (event) => {
     const file = event.target.files[0];
@@ -53,31 +73,59 @@ function App() {
     });
   }
 
+  const onRowEditingStarted = useCallback((event) => {
+    console.log('never called - not doing row editing');
+  }, []);
+
+  const onRowEditingStopped = useCallback((event) => {
+    console.log('never called - not doing row editing');
+  }, []);
+
+  const onCellEditingStarted = useCallback((event) => {
+    console.log('cellEditingStarted');
+  }, []);
+
+  const onCellEditingStopped = useCallback((event) => {
+    console.log('cellEditingStopped');
+  }, []);
+
   return (
     <div className="App">
-      <div class="container my-3">
+      <div class="container">
         <br></br>
-        <input 
-            type="file"
-            name="file"
-            accept=".csv,.xlsx"
-            onChange={handleFile}
-            style = {{display:"block"}}
-        ></input>
-        
-      </div>
-
+        <div class="row">
+          <div class="col">
+            <input 
+                class="form-control"
+                type="file"
+                name="file"
+                accept=".csv,.xlsx"
+                onChange={handleFile}
+                style = {{display:"block"}}
+            ></input> 
+          </div>
+          <div class="col-sm">
+          <button
+              onClick={handleOnExport}
+              style={{ marginBottom: '5px', fontWeight: 'bold' }}
+            >
+              Export to Excel
+            </button>
+        </div>
+        </div>
+      </div>  
       <br></br>
-      <br></br>
-      <div className='ag-theme-alpine-dark' style={{height:500, width:2000}}>
+      <div className='ag-theme-alpine' style={{height:800, width:2000}}>
           <AgGridReact 
             rowData={items}
             columnDefs={columns}
+            defaultColDef={defaultColDef}
+            onRowEditingStarted={onRowEditingStarted}
+            onRowEditingStopped={onRowEditingStopped}
+            onCellEditingStarted={onCellEditingStarted}
+            onCellEditingStopped={onCellEditingStopped}
           ></AgGridReact>
       </div>
-   
-    
-
     </div>
   );
 }
